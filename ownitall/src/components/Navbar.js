88a6691213a2531118a0f06535d35ae9d438e08b
JovanMilutinovicGit <milutinovicjovan248@gmail.com>
2022-10-16 13:40:13 +0200

    mobile menu

--- ownitall/src/components/Navbar.js
+++ ownitall/src/components/Navbar.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "../pages/HomePage.js";
 import logo from "../assets/LogoOwnItAll.png";
 import "../Styles/Navbar.css";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div className="navbar">
       <div className="left-side">
-        <img src={logo} />
+        <Link to="/">
+          <img src={logo} />
+        </Link>
       </div>
-      <div className="right-side">
+      <div className={!openMenu ? "right-side" : "mobile-menu"}>
         <Link to="/" className="link">
           Home
         </Link>
@@ -23,11 +27,18 @@ const Navbar = () => {
         <Link to="/contact" className="link">
           Contact
         </Link>
-        <Link to="/booking" id="btn">
-          Book Now
-        </Link>
+        <div id="btn">
+          <Link to="/booking" className="bookBtn">
+            Book Now
+          </Link>
+        </div>
       </div>
-      <i class="fa fa-bars"></i>
+      <i
+        class="fa fa-bars"
+        onClick={() => {
+          setOpenMenu(!openMenu);
+        }}
+      ></i>
     </div>
   );
 };
